83399d9711e330e3d786e5468bbbbca346248911
aatashj1 <frnkxoxo69@gmail.com>
2023-05-08 21:59:47 +0300

    fix: code without Synchronous API

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,47 +1,37 @@
 const fs = require('fs');
 const path = require('path');
-const readline = require('readline');
+const { stdin, stdout } = process;
 
 const filePath = path.join(__dirname, 'text.txt');
 
-// Create a readline interface for getting user input
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-// Check if the file already exists, and create it if it doesn't
-if (!fs.existsSync(filePath)) {
-  fs.writeFileSync(filePath, '');
-}
-
-// Prompt the user to enter text
-rl.setPrompt('put some txt HERE: ');
-
-// Display the prompt
-rl.prompt();
-
-// Listen for user input
-rl.on('line', (input) => {
-  // If the user enters 'exit', close the readline interface and exit the program
-  if (input === 'exit') {
-    rl.close();
-    console.log('okEEy bye!!');
-    process.exit();
+fs.access(filePath, (err) => {
+  if (err) {
+    fs.writeFile(filePath, '', (err) => {
+      if (err) throw err;
+      startListening();
+    });
+  } else {
+    startListening();
   }
+});
 
-  // Otherwise, append the input to the file
-  fs.appendFile(filePath, input + '\n', (err) => {
-    if (err) throw err;
+function startListening() {
+  stdout.write('your text: ');
+
+
+  stdin.on('data', (input) => {
+    input = input.toString().trim();
+
+    if (input === 'exit') {
+      stdout.write('bye!\n');
+      stdin.end();
+      process.exit();
+    }
+
+    fs.appendFile(filePath, input + '\n', (err) => {
+      if (err) throw err;
+    });
+
+    stdout.write('your text: ');
   });
-
-  // Display the prompt again
-  rl.prompt();
-});
-
-// Listen for SIGINT (ctrl+c) and close the readline interface
-rl.on('SIGINT', () => {
-  rl.close();
-  console.log('okEEy bye!');
-  process.exit();
-});
+}

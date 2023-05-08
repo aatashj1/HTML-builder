f6239bf435b1daaabab32412f1e060988b9d37de
aatashj1 <frnkxoxo69@gmail.com>
2023-05-08 21:12:46 +0300

    feat: solve for 01 task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,6 +2,6 @@ const FS = require('fs');
 const PATH = require('path');
 
 let inputFromTxt = FS.createReadStream(PATH.join(__dirname, 'text.txt'), 'utf8');
-let res = '';
+let res = ' ';
 
 inputFromTxt.on('data', data => console.log(data));
